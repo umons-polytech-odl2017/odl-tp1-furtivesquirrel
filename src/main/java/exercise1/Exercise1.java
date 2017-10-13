910dc8c19eb2a368822854d96a778c98953b30f7
150586 <terry.weber96@outlook.be>
2017-10-13 15:45:27 +0200

    J'ai fini la classe Person

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -2,7 +2,7 @@ package exercise1;
 
 public class Exercise1 {
 	static Person createPerson(String name, int age) {
-		// Ajoutez les champs name et age à la classe Person.
+
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
 		return null;
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,14 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+	public Person(){
+		age=0;
+		nom=anonyme;
+	}
+
+	public int getAge() {return age;}
+	public String getName () {return name;}
 }
